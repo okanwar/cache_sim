8fd0a799dd92b7ad016ce08ae7f8631ed3d3d7fa
sat <sat@sandiego.edu>
2017-10-29 22:14:51 -0700

    Added getopt code for s, v, and t options and provided more guidance on code structure.

--- csim.c
+++ csim.c
@@ -4,12 +4,90 @@
  * Fill in file header comment with your name(s) and a short paragraph about
  * what your program does.
  */
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "cachelab.h"
 
 typedef unsigned long int mem_addr;
 
-int main()
-{
-    printSummary(0, 0, 0);
+// forward declaration
+void simulateCache(char *trace_file, int num_sets, int block_size, int lines_per_set, int verbose);
+
+/**
+ * Prints out a reminder of how to run the program.
+ *
+ * @param executable_name Strign containing the name of the executable.
+ */
+void usage(char *executable_name) {
+	printf("%s -t <trace> -s <set bits> -E <lines per set> -b <block offset bits> [-v]", executable_name);
+}
+
+int main(int argc, char *argv[]) {
+
+	int verbose_mode = 0;
+	int num_sets = 2;
+	char *trace_filename = NULL;
+	
+	opterr = 0;
+
+	// TODO: update this to support the b and E options
+	int c = -1;
+	while ((c = getopt(argc, argv, "vs:t:")) != -1) {
+		switch (c) {
+			case 'v':
+				// enable verbose mode
+				verbose_mode = 1;
+				break;
+			case 's':
+				// specify the number of sets
+				num_sets = 1 << strtol(optarg, NULL, 10);
+				break;
+			case 't':
+				// specify the trace filename
+				trace_filename = optarg;
+				break;
+			case '?':
+			default:
+				usage(argv[0]);
+				exit(1);
+		}
+	}
+
+	if (verbose_mode) {
+		printf("Verbose mode enabled.\n");
+		printf("Trace filename: %s\n", trace_filename);
+		printf("Number of sets: %d\n", num_sets);
+	}
+
+	simulateCache(trace_filename, num_sets, 2, 1, verbose_mode);
+
     return 0;
+}
+
+/**
+ * Simulates cache with the specified organization (S, E, B) on the given
+ * trace file.
+ *
+ * @param trace_file Name of the file with the memory addresses.
+ * @param num_sets Number of sets in the simulator.
+ * @param block_size Number of bytes in each cache block.
+ * @param lines_per_set Number of lines in each cache set.
+ * @param verbose Whether to print out extra information about what the
+ *   simulator is doing (1 = yes, 0 = no).
+ */
+void simulateCache(char *trace_file, int num_sets, int block_size, 
+						int lines_per_set, int verbose) {
+	// Variables to track how many hits, misses, and evictions we've had so
+	// far during simulation.
+	int hit_count = 0;
+	int miss_count = 0;
+	int eviction_count = 0;
+
+	// TODO: This is where you will fill in the code to perform the actual
+	// cache simulation. Make sure you split your work into multiple functions
+	// so that each function is as simple as possible.
+
+    printSummary(hit_count, miss_count, eviction_count);
 }
