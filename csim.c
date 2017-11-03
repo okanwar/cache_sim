cc8e389cfaa240b77c3b6f0bee58a79dac9e218f
Michael Dana <mdana@sandiego.edu>
2017-11-03 11:56:29 -0700

    continued working on simulating cache, specifically chechink valid bits andupdating based on tag

--- csim.c
+++ csim.c
@@ -16,6 +16,7 @@ typedef unsigned long int mem_addr;
 // forward declaration
 void simulateCache(char *trace_file, int num_sets, int block_size, int lines_per_set, int verbose);
 void addressCalc(mem_addr addy, int *tag, int *set, int block_bits, int tag_bits, int set_bits);
+void updateLRU();
 /**
  * Prints out a reminder of how to run the program.
  *
@@ -106,6 +107,10 @@ void simulateCache(char *trace_file, int num_sets, int block_size,
 	int set = 0;
 	int tag = 0;
 	char instruction;
+	int *cache;
+	int tag_index = 0;
+
+	cache = (int*)calloc( num_sets * lines_per_set * 2 , sizeof(int) );
 
 	FILE *trace_f;
 	trace_f = fopen(trace_file, "r");
@@ -114,6 +119,27 @@ void simulateCache(char *trace_file, int num_sets, int block_size,
 		addressCalc(addy, &tag, &set, block_size, 64, lines_per_set);
 
 		//Simulate Cache
+		
+		//Iterates over the set the address is in cheching valid bit and tag
+		for( int i = (3 * set * lines_per_set); i < (3 * (set+1)* lines_per_set); i+=3 ){
+			
+			// i is valid bit index
+			// i+1 is tag index
+
+			if( cache[i] == 0 ){ //Store in cache if valid bit is not set
+				cache[i] = 1;
+				cache[i+1] = tag;
+				miss_count++;
+			} else if ( cache[i] == 1){ //Check tag if valid bit is set
+				if( cache[+1] == tag ){
+					hit_count++;
+				}
+
+			}	
+		}
+
+		//Evicte lru 
+
 	}	
 
 
@@ -129,4 +155,9 @@ void addressCalc(mem_addr addy, int *tag, int *set, int block_bits, int tag_bits
 	set_bits = log(set_bits) / log(2);
 	*set = ((addy << tag_bits) >> (tag_bits + block_bits));
 	*tag = (addy >> (set_bits + block_bits));
+}
+
+
+void updateLRU(){
+
 }
